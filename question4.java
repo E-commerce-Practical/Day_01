bbc7373a5beaeec6b9b3988e013f9ada0ff92ca3
Pasindu Nuwan <154910776+Pasindunuwan2000@users.noreply.github.com>
2025-03-16 14:57:35 +0530

    feat: add program to print odd numbers between 1 to 20

---
+++ question4.java
@@ -0,0 +1,8 @@
+public class question4 {
+    public static void main(String[] args){
+       // 4. Print odd numbers between 1 to 20
+       for (int i = 1; i <= 20; i += 2) {
+        System.out.println(i) ;
+        }
+    }
+}
