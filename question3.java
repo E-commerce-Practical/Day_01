f65bc475d8de7192ed25ee673f3121fb2bb22b5e
Pasindu Nuwan <154910776+Pasindunuwan2000@users.noreply.github.com>
2025-03-16 12:31:33 +0530

    feat: add program to print numbers in reverse from 10 to 1

---
+++ question3.java
@@ -0,0 +1,8 @@
+public class question3 {
+    public static void main(String[] args){
+        // 3. Print numbers in reverse from 10 to 1
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
